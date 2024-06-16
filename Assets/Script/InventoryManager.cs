b35c6e47ba0b540ce4b37a2d3ce1859a7fa3fa8c
Nik Muhammad Izzuan <46263649+Nikizzuan@users.noreply.github.com>
2024-06-16 10:09:45 +0800

    Inventory System
    -Implemented a basic InventoryManager and item data structure, which will be enhanced throughout the development.
    
    -Created ScriptableObjects to store item information, acting like a database within the game itself.

---
+++ Assets/Script/InventoryManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+     private static InventoryManager _instance;
+
+    public static InventoryManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<InventoryManager>();
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("InventoryManager");
+                    _instance = go.AddComponent<InventoryManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private List<Item> items = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+    }
+
+    public bool HasItem(Item item)
+    {
+        return items.Contains(item);
+    }
+}
